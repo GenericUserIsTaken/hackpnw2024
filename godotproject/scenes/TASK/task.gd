2477bd43d109559ef066e392d4f10b155bd154ea
Reese G <86173015+valsei@users.noreply.github.com>
2024-03-02 15:01:01 -0800

    started task pushing each other

--- godotproject/scenes/TASK/task.gd
+++ godotproject/scenes/TASK/task.gd
@@ -4,6 +4,7 @@ class_name Task
 @export var children: Array = []
 @export var parent: Task
 @export var taskname: String = "TextName"
+@export var nearbyTasks: Array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,3 +17,12 @@ func changeTaskName(newName : String):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_area_2d_area_entered(area):
+	if (area.parent != self):
+		nearbyTasks.append(area.parent)
+
+
+func _on_area_2d_area_exited(area):
+	pass # Replace with function body.
