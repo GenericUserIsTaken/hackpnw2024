604221cccccf654aee7e5b49a33d8c4f6cc2d6e2
Reese G <86173015+valsei@users.noreply.github.com>
2024-03-02 18:52:39 -0800

    fixed changename method

--- godotproject/scenes/TASK/task.gd
+++ godotproject/scenes/TASK/task.gd
@@ -14,10 +14,12 @@ var otherTasks
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var names = ["amogus","sussy","goduh","nah","ayo wtf","top ten","task: pet maxwell"]
+	self.changeTaskName(names.pick_random())
 
 func changeTaskName(newName : String):
-	$Label.text = newName
+	#if $Label != null:
+	$CenterContainer/Label.text = newName
 	taskname = newName
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
