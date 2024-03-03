e2677296cfb0ba8eddb8947e16b19cad24e5afe7
Reese G <86173015+valsei@users.noreply.github.com>
2024-03-03 00:37:25 -0800

    reworked the tasknode script
    having issues with $ get child thing

--- godotproject/scenes/TASK/task.gd
+++ godotproject/scenes/TASK/task.gd
@@ -1,29 +1,29 @@
 extends RigidBody2D
 
 class_name Task
-@export var children: Array = []
 @export var parentTask: Task
 @export var taskname: String = "TextName"
-@export var nearbyTasks: Dictionary = {}
 
-# an extra multiplier on push to others
-@export var pushWeight: float = 1
+# motion of the task
+var velo = Vector2()
+var maxVelo = 0
+var accel = Vector2()
+@export var friction: float = 0
 
-# higher is more resistance
-@export var pushResistance: float = 0
+# multiplier for all the ranges. should be between 0 and 1
+@export var taskSize: float = 1
 
-# is taken from the collider radius
-@export var maxDistanceForPush: float = 250
+# motion ranges and multipliers
+var allPushRange = 500
+var allPushMultiplier = 1
 
-@export var distanceToForceBaseMultiplier: float = 0.1
+var parentPushRange = 150
+var parentPushMultiplier = 20
 
-@export var parentPullMultiplier: float = 10
+# no range, always applied
+var parentPullMultiplier = 20
 
-@export var parentPushMultiplier: float = 20
-
-@export var friction: float = 30
-
-# tracks other tasks within push distance
+# tracks other tasks within range distance
 var otherTasks
 
 var parentForce = Vector2(0,0)
@@ -34,12 +34,49 @@ func _ready():
 	var names = ["amogus","sussy","goduh",":3","nah","ayo wtf","uwu","top ten","task: pet maxwell"]
 	self.changeTaskName(names.pick_random())
 	
-	$Area2D/NearbyCollider.shape.radius = maxDistanceForPush
+	# change push ranges based on task size
+	allPushRange *= taskSize
+	parentPushRange *= taskSize
+	
+	#self.rigidbody2d.set_linear_damp(friction)
+	
+	#$Area2D/NearbyCollider
 
 func changeTaskName(newName : String):
-	$CenterContainer/Label.text = newName
+	#$CenterContainer/Label.text = newName
 	taskname = newName
 
+# handles the motion of the tasknode
+func _integrate_forces(state : PhysicsDirectBodyState2D):
+	# get all the overlapping areas
+	otherTasks = $Area2D.get_overlapping_areas()
+	
+	# calculate incoming acceleration
+	# push from all:
+	accel = Vector2()
+	for otherTask in otherTasks:
+		var diffVector = self.global_position - otherTask.global_position
+		accel += diffVector.normalized() * max(0, otherTask.get_parent().allPushRange - diffVector.length()) * allPushMultiplier
+	# if has a parent, apply linking forces
+	if parentTask != null:
+		var diffVector = self.global_position - parentTask.global_position
+		# pull from parent:
+		accel -= diffVector * parentPullMultiplier
+		# push from parent:
+		accel += diffVector.normalized() * max(0, parentTask.parentPushRange - diffVector.length()) * parentPushMultiplier
+	
+	# calculate next velocity using acceleration
+	velo += accel
+	if velo.length() <= friction:
+		velo = Vector2()
+	else:
+		velo = velo.normalize() * (velo.length() - friction)
+	
+	# apply velocity
+	state.set_linear_velocity(velo)
+	queue_redraw()
+
+"""
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	otherTasks = $Area2D.get_overlapping_areas()
@@ -63,13 +100,13 @@ func calc_pull_to_parent():
 	diffVector *= parentPullMultiplier * distanceToForceBaseMultiplier
 	diffVector *= parentTask.pushWeight
 	return diffVector
+"""
 
 func _draw():
 	# draw lines to parent
 	if parentTask != null:
 		draw_line(Vector2(0,0), self.to_local(parentTask.global_position), Color.BLACK, 10.0)
 		
-	# draw velo debug
-	for otherTask in otherTasks:
-		draw_line(Vector2(0,0), calc_push_from(otherTask), Color.RED, 4.0)
-	draw_line(Vector2(0,0), parentForce, Color.GREEN, 4.0)
+	# draw velo and accel debug
+	draw_line(Vector2(0,0), velo, Color.BLUE, 4.0)
+	draw_line(Vector2(0,0), accel, Color.GREEN, 4.0)
